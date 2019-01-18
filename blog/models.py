1549c4985ef10d83c774cde693540050af59b25a
pear9123 <pear9123@gmail.com>
2019-01-18 19:25:27 +0900

    로그인,로그아웃,게시물 구현

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField("제목", max_length=100)
+    slug = models.SlugField("Slug", unique=True)
+    description = models.CharField("설명", max_length=200, blank=True)
+    content = models.TextField("본문")
+    photo = models.ImageField(blank=True, null=True)
+    create_date = models.DateTimeField("작성시간", auto_now_add=True)
+    modify_date = models.DateTimeField("수정시간", auto_now=True)
+
+    def __str__(self):
+        return self.title
