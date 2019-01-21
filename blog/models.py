d8f23754b18129ec1ada062c41aea87c349b71e9
pear9123 <pear9123@gmail.com>
2019-01-22 00:36:54 +0900

    글 작성자 생성

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 # Create your models here.
 class Post(models.Model):
     title = models.CharField("제목", max_length=100)
-#    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     slug = models.SlugField("Slug", unique=True)
     description = models.CharField("설명", max_length=200, blank=True)
     content = models.TextField("본문")
@@ -13,4 +13,4 @@ class Post(models.Model):
     modify_date = models.DateTimeField("수정시간", auto_now=True)
 
     def __str__(self):
-        return self.title
+        return f'Post (PK: {self.pk}, Author: {self.author.username})'
